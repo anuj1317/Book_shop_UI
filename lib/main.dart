6f7ce4cf2d54de8516e2b465a5c759fffa1dd28d
Anuj Singh <anujsingh1317@gmail.com>
2021-01-07 14:40:14 +0530

    Added Details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'details.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -130,123 +132,131 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildListItem(String imgPath, String bookName, String price) {
-    return Padding(
-        padding: EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
-        child: Container(
-            height: 200.0,
-            width: 200.0,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 6.0,
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 5.0)
-                ]),
-            child: Stack(
-              children: <Widget>[
-                Container(
-                    height: 175.0,
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.white, Color(0xFFACBEA3)],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight
-                        ),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(10.0),
-                            topRight: Radius.circular(10.0)
-                        )
-                    )
-                ),
-                Hero(
-                    tag: imgPath,
-                    child: Container(
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push((MaterialPageRoute(
+            builder: (context) => Details(heroTag: imgPath, BookName: bookName, BookPrice: price)
+        )));
+      },
+
+      child: Padding(
+          padding: EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
+          child: Container(
+              height: 200.0,
+              width: 200.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 6.0,
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 5.0)
+                  ]),
+              child: Stack(
+                children: <Widget>[
+                  Container(
                       height: 175.0,
                       decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(imgPath),
-                              fit: BoxFit.contain
+                          gradient: LinearGradient(
+                              colors: [Colors.white, Color(0xFFACBEA3)],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight
                           ),
                           borderRadius: BorderRadius.only(
                               topLeft: Radius.circular(10.0),
                               topRight: Radius.circular(10.0)
                           )
-                      ),
-                    )
-                ),
-                Positioned(
-                    top: 160.0,
-                    right: 20.0,
-                    child: Material(
-                        elevation: 2.0,
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: Container(
-                          height: 30.0,
-                          width: 30.0,
-                          child: Center(
-                              child: Icon(Icons.favorite, color: Colors.red, size: 17.0)
-                          ),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15.0),
-                              color: Colors.white
-                          ),
-                        )
-                    )
-                ),
-                Positioned(
-                    top: 190.0,
-                    left: 10.0,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(bookName,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
-                                fontSize: 14.0)),
-                        SizedBox(height: 3.0),
-                        Container(
-                            width: 175.0,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Text(
-                                      '4.9',
-                                      style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 12.0),
-                                    ),
-                                    SizedBox(width: 3.0),
-                                    Icon(Icons.star,
-                                        color: Colors.green, size: 14.0),
-                                    Icon(Icons.star,
-                                        color: Colors.green, size: 14.0),
-                                    Icon(Icons.star,
-                                        color: Colors.green, size: 14.0),
-                                    Icon(Icons.star,
-                                        color: Colors.green, size: 14.0),
-                                    Icon(Icons.star,
-                                        color: Colors.green, size: 14.0),
-                                  ],
-                                ),
-                                Text(price,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.black,
-                                        fontSize: 16.0)),
-                              ],
+                      )
+                  ),
+                  Hero(
+                      tag: imgPath,
+                      child: Container(
+                        height: 175.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(imgPath),
+                                fit: BoxFit.contain
+                            ),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10.0),
+                                topRight: Radius.circular(10.0)
                             )
-                        )
-                      ],
-                    )
-                )
-              ],
-            )
-        )
+                        ),
+                      )
+                  ),
+                  Positioned(
+                      top: 160.0,
+                      right: 20.0,
+                      child: Material(
+                          elevation: 2.0,
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Container(
+                            height: 30.0,
+                            width: 30.0,
+                            child: Center(
+                                child: Icon(Icons.favorite, color: Colors.red, size: 17.0)
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15.0),
+                                color: Colors.white
+                            ),
+                          )
+                      )
+                  ),
+                  Positioned(
+                      top: 190.0,
+                      left: 10.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(bookName,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                  fontSize: 14.0)),
+                          SizedBox(height: 3.0),
+                          Container(
+                              width: 175.0,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        '4.9',
+                                        style: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 12.0),
+                                      ),
+                                      SizedBox(width: 3.0),
+                                      Icon(Icons.star,
+                                          color: Colors.green, size: 14.0),
+                                      Icon(Icons.star,
+                                          color: Colors.green, size: 14.0),
+                                      Icon(Icons.star,
+                                          color: Colors.green, size: 14.0),
+                                      Icon(Icons.star,
+                                          color: Colors.green, size: 14.0),
+                                      Icon(Icons.star,
+                                          color: Colors.green, size: 14.0),
+                                    ],
+                                  ),
+                                  Text(price,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black,
+                                          fontSize: 16.0)),
+                                ],
+                              )
+                          )
+                        ],
+                      )
+                  )
+                ],
+              )
+          )
+      ),
     );
   }
 }
